58d0d23753af4601a61aff1ebb52ded99fe33dac
amanda-esp <amanda.635@aluno.ueg.br>
2026-02-28 19:41:09 -0300

    Add asynchronous confrontation and power-up logic

--- src/index.js
+++ src/index.js
@@ -64,6 +64,64 @@ async function getRandomBlock(){
     }
     return roundBlock
 };
+async function getTypeConfront() {
+    let type = Math.random()
+    let confrontType
+    if(type < 0.5)
+        confrontType = "casco"
+    else
+        confrontType = "bomba"
+    return confrontType
+}
+                                                                                                    
+async function randomPowerUp() {
+    let type = Math.random()
+    let powerUp
+    if(type < 0.5)        
+        powerUp = true
+    else
+        powerUp = false
+    return powerUp
+}
+
+async function powerConfront(character1, character2,powerPlayer1, powerPlayer2) {
+    
+    let confrontType = await getTypeConfront()
+    let winner, loser, flag = false
+
+    if(powerPlayer1 > powerPlayer2){
+        winner = character1
+        loser = character2
+    }
+    else if(powerPlayer2 > powerPlayer1){
+        winner = character2
+        loser = character1
+    }
+    else{
+        console.log("EMPATE! Nenhum ponto foi perdido!")
+        flag = true
+    }   
+    
+    if(!flag){
+        if(confrontType === "casco"){
+            console.log("Tipo de confronto: Casco 🐢")
+            console.log(`${winner.NOME} venceu o confronto e ${loser.NOME} perdeu 1 ponto!`)
+            loser.PONTOS > 0 ? loser.PONTOS-- : ""
+        }else{
+            console.log("Tipo de confronto: Bomba 💣")
+            console.log(`${winner.NOME} venceu o confronto e ${loser.NOME} perdeu 2 pontos!`)
+            loser.PONTOS > 1 ? loser.PONTOS -= 2 : loser.PONTOS = 0
+        }
+
+        let powerUp = await randomPowerUp()
+        if(powerUp){
+            console.log(`\n${winner.NOME} está com sorte e ganhou um turbo! +1 ponto`)
+            winner.PONTOS++
+        }
+    }
+
+    
+}
 
 async function rollResult(character, block, diceResult,value) {
     console.log(`${character} rolou um dado de ${block} valor ${diceResult} + ${value} = ${diceResult+value}`)
@@ -83,38 +141,36 @@ async function playRaceEngine(character1, character2) {
         if(block === "RETA➡️"){
             await rollResult(character1.NOME,"velocidade",dicePlayer1,character1.VELOCIDADE)
             await rollResult(character2.NOME,"velocidade",dicePlayer2,character2.VELOCIDADE)
+            console.log("")
             totalPlayer1 = dicePlayer1 + character1.VELOCIDADE
             totalPlayer2 = dicePlayer2 + character2.VELOCIDADE
             
         } else if (block === "CURVA⤴️"){
             await rollResult(character1.NOME,"manobrabilidade",dicePlayer1,character1.MANOBRABILIDADE)
             await rollResult(character2.NOME,"manobrabilidade",dicePlayer2,character2.MANOBRABILIDADE)
+            console.log("")
             totalPlayer1 = dicePlayer1 + character1.MANOBRABILIDADE
             totalPlayer2 = dicePlayer2 + character2.MANOBRABILIDADE
         } else {
-            console.log(`Confronto entre ${character1.NOME} e ${character2.NOME}!`)
-            let powerPlayer1 = dicePlayer1 + character1.PODER
-            let powerPlayer2 = dicePlayer2 + character2.PODER
+            console.log(`Confronto entre ${character1.NOME} e ${character2.NOME}!\n`)
+            
             await rollResult(character1.NOME,"poder",dicePlayer1,character1.PODER)
             await rollResult(character2.NOME,"poder",dicePlayer2,character2.PODER)
-            if(powerPlayer1 > powerPlayer2){
-                console.log(`${character1.NOME} venceu a rodada! ${character2.NOME} perde 1 ponto`)
-                character2.PONTOS > 0 ? character2.PONTOS-- : ""
-            }else if(powerPlayer2 > powerPlayer1){
-                console.log(`${character2.NOME} venceu a rodada! ${character1.NOME} perde 1 ponto`)
-                character1.PONTOS > 0 ? character1.PONTOS-- : ""
-            }else
-                console.log("EMPATE! Nenhum ponto foi perdido!")
+            console.log("")
+            let powerPlayer1 = dicePlayer1 + character1.PODER
+            let powerPlayer2 = dicePlayer2 + character2.PODER
+            
+            await powerConfront(character1, character2,powerPlayer1, powerPlayer2)
         }
 
         if(totalPlayer1 > totalPlayer2){
             console.log(`${character1.NOME} ganhou + 1 ponto!`)
             character1.PONTOS++;
         }
-        if(totalPlayer2 > totalPlayer1){
+        else if(totalPlayer2 > totalPlayer1){
             console.log(`${character2.NOME} ganhou + 1 ponto!`)
             character2.PONTOS++;
-        }    
+        }
              
         console.log("=======================================================")
 
@@ -128,16 +184,70 @@ async function declareWinner(character1,character2) {
     console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)\n`)
 
     if(character1.PONTOS > character2.PONTOS)
-        console.log(`${character1.NOME} é o vencedor!🏆\n`)
+        console.log(`${character1.NOME} é o/a vencedor/a!🏆\n`)
     else if (character1.PONTOS < character2.PONTOS)
-        console.log(`${character2.NOME} é o vencedor!🏆\n`)
+        console.log(`${character2.NOME} é o/a vencedor/a!🏆\n`)
     else
         console.log(`A partida terminou em empate!!\n`)
 }
 
 //Auto invoke function: a função irá executar toda vez q o arquivo for chamado
 (async function main(){
-    console.log("")
+    console.log("=======================================================")
+    console.log("   🏁🚩 Bem-vindos a corrida de Mario Kart 🚩🏁")
+    console.log("=======================================================\n")
+    console.log("Escolha os competidores:\n")
+    console.log("1 - Mario\n\tVelocidade: 4\n\tManobrabilidade: 3\n\tPoder: 3\n")
+    console.log("2 - Peach\n\tVelocidade: 3\n\tManobrabilidade: 4\n\tPoder: 2\n")
+    console.log("3 - Yoshi\n\tVelocidade: 3\n\tManobrabilidade: 3\n\tPoder: 4\n")
+    console.log("4 - Bowser\n\tVelocidade: 5\n\tManobrabilidade: 2\n\tPoder: 5\n")
+    console.log("5 - DonkeyKong\n\tVelocidade: 2\n\tManobrabilidade: 2\n\tPoder: 5\n")
+    console.log("6 - Luigi\n\tVelocidade: 3\n\tManobrabilidade: 4\n\tPoder: 2\n")
+    const prompt = require('prompt-sync')();
+    let player1, player2, choice1, choice2
+    do{
+        choice1 = prompt("Jogador 1: ")
+        switch(choice1){
+            case "1": player1 = Mario
+            break;
+            case "2": player1 = Peach
+            break;
+            case "3": player1 = Yoshi
+            break;
+            case "4": player1 = Bowser
+            break;
+            case "5": player1 = DonkeyKong
+            break;
+            case "6": player1 = Luigi
+            break;
+            default: console.log("Escolha inválida para jogador 1, tente novamente!")
+            break;
+        }
+    }while(!player1)
+    do{
+        do{
+            choice2 = prompt("Jogador 2: ")
+            if(choice1 === choice2)
+            console.log("Jogadores não podem escolher o mesmo personagem, tente novamente!")
+        }while(choice1 === choice2)
+        
+        switch(choice2){
+            case "1": player2 = Mario
+            break;
+            case "2": player2 = Peach
+            break;
+            case "3": player2 = Yoshi
+            break;
+            case "4": player2 = Bowser
+            break;
+            case "5": player2 = DonkeyKong
+            break;
+            case "6": player2 = Luigi
+            break;
+            default: console.log("Escolha inválida para jogador 2, tente novamente!")
+            break;
+        }
+    }while(!player2)
     console.log("=======================================================")
     console.log(`   🏁🚩 Corrida entre ${player1.NOME} e ${player2.NOME} começando 🚩🏁`)
     console.log("=======================================================\n")
